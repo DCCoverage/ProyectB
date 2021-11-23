f5fb754aaf28b239e2d850bd65ec8d98aff95c85
Jeronimo <jcsalazar@uc.cl>
2021-11-23 20:52:45 -0300

    new project

--- app/javascript/packs/vue_pack.js
+++
@@ -1,8 +0,0 @@
-import TurbolinksAdapter from "vue-turbolinks";
-import Vue from "vue";
-import router from "../router";
-
-const app = Vue.createApp({});
-app.use(TurbolinksAdapter);
-app.use(router);
-app.mount("#app");
--- app/javascript/views/Index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <main>Hello world!</main>
-</template>
