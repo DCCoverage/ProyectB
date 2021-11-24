fddb96aef6b8bc6d65f61e1f958ee6d3e2593f9e
Nicolas <naquiroz@uc.cl>
2021-11-23 23:46:15 -0300

    Add showtimes

---
+++ app/views/show_times/_show_time.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! show_time, :id, :created_at, :updated_at
+json.url show_time_url(show_time, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :show_times
   resources :movies
   root to: 'hello#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
