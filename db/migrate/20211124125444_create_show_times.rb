a6ed8f1fcb8595891343b9df93afb14ff238db08
Nicolas <naquiroz@uc.cl>
2021-11-24 09:54:53 -0300

    Add showtime scaffold

---
+++ db/migrate/20211124125444_create_show_times.rb
@@ -0,0 +1,11 @@
+class CreateShowTimes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :show_times do |t|
+      t.belongs_to :movie, null: false, foreign_key: true
+      t.belongs_to :room, null: false, foreign_key: true
+      t.string :when
+
+      t.timestamps
+    end
+  end
+end
