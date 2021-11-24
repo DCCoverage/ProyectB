10f33ef4bf2bb97ab440bf5ed045e53bc7c506ba
Nicolas <naquiroz@uc.cl>
2021-11-23 23:44:08 -0300

    Add movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root :to => "hello#index"
+  resources :movies
+  root to: 'hello#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
