55d36060a3cb6eff01847f18e8100a5b84cf35c9
Jeronimo <jcsalazar@uc.cl>
2021-11-28 14:06:04 -0300

    fix: update tests to not depend on messages

--- spec/create_movie_spec.rb
+++ spec/create_movie_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe 'Create Movie', type: :system do
     let! (:movie_description) {'La pelicula marca el regreso de Eddie Brock (Tom Hardy), el astuto periodista y reportero que, despues de quedar infectado, experimento radicales cambios en su cuerpo y adquirio los poderes del simbionte Venom, que usa a Brock como huesped y le convertio en un despiadado y peligroso supervillano. En esta ocasion, Venom se reencontrara con Cletus Cassady, mas conocido como Carnage, el enemigo mas sangriento de la historia de Spider-Man.' }
     it 'creates a movie' do
         visit(new_movie_path)
+        expect(Movie.all.count).to be(0)
         fill_in 'movie_name', with: movie_name
         fill_in 'movie_description', with: movie_description
         attach_file('movie_image', "#{Rails.root}/spec/fixtures/movie_image_test.jpg")
         click_button('Crear película')
-        expect(page).to have_content('Movie was successfully created.')
+        expect(Movie.all.count).to be(1)
     end
 
     context 'existing movie' do
